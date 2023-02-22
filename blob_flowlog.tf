3e97028a6862b9c066fee5595035c5eb0b734af2
Douglas Francis <Douglas@PoshOps.io>
2023-02-22 16:30:02 -0600

    Update blob_flowlog.tf

--- blob_flowlog.tf
+++ blob_flowlog.tf
@@ -46,7 +46,7 @@ resource "azurerm_storage_account_customer_managed_key" "enable_flowlogs_cmk" {
 }
 
 module "diag_flowlogs_sa" {
-  source                = "git@github.com:Coalfire-CF/ACE-Azure-Diagnostics.git?ref=v1.0.1"
+  source                = "github.com/Coalfire-CF/ACE-Azure-Diagnostics?ref=latest"
   diag_log_analytics_id = var.diag_log_analytics_id
   resource_id           = azurerm_storage_account.flowlogs.id
   resource_type         = "sa"

b0eaef7ae10745a3c3cbf1bb33d9eb163671d39f
Douglas Francis <douglas@poshops.io>
2023-06-15 14:30:05 -0400

    testing sa changes

--- blob_docs.tf
+++ blob_docs.tf
@@ -1,29 +1,29 @@
-# Storage account to put FedRAMP archive documents into
-resource "azurerm_storage_account" "docs_sa" {
-  name                            = length("${local.storage_name_prefix}archivesa") <= 24 ? "${local.storage_name_prefix}archivesa" : "${var.location_abbreviation}mp${var.app_abbreviation}archivesa"
-  resource_group_name             = azurerm_resource_group.management.name
-  location                        = var.location
-  account_tier                    = "Standard"
-  account_replication_type        = "GRS"
-  min_tls_version                 = "TLS1_2"
-  enable_https_traffic_only       = true
-  allow_nested_items_to_be_public = false
+# # Storage account to put FedRAMP archive documents into
+# resource "azurerm_storage_account" "docs_sa" {
+#   name                            = length("${local.storage_name_prefix}archivesa") <= 24 ? "${local.storage_name_prefix}archivesa" : "${var.location_abbreviation}mp${var.app_abbreviation}archivesa"
+#   resource_group_name             = azurerm_resource_group.management.name
+#   location                        = var.location
+#   account_tier                    = "Standard"
+#   account_replication_type        = "GRS"
+#   min_tls_version                 = "TLS1_2"
+#   enable_https_traffic_only       = true
+#   allow_nested_items_to_be_public = false
 
-  identity {
-    type = "SystemAssigned"
-  }
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      customer_managed_key # required by https://github.com/hashicorp/terraform-provider-azurerm/issues/16085
-    ]
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#     ignore_changes = [
+#       customer_managed_key # required by https://github.com/hashicorp/terraform-provider-azurerm/issues/16085
+#     ]
+#   }
 
-  tags = merge({
-    Function = "Storage"
-    Plane    = "Management"
-  }, var.global_tags, var.regional_tags)
-}
+#   tags = merge({
+#     Function = "Storage"
+#     Plane    = "Management"
+#   }, var.global_tags, var.regional_tags)
+# }
 
 resource "azurerm_storage_container" "docs_container" {
   name                  = "fedrampdocsandartifacts"
@@ -55,4 +55,25 @@ module "diag_docs_sa" {
   diag_log_analytics_id = var.diag_log_analytics_id
   resource_id           = azurerm_storage_account.docs_sa.id
   resource_type         = "sa"
+}
+
+module "docs_sa" {
+  source                = "github.com/Coalfire-CF/ACE-Azure-StorageAccount"
+  name                  = "${replace(var.resource_prefix, "-", "")}docs"
+  resource_group_name   = azurerm_resource_group.management.name
+  location              = var.location
+  account_kind          = "StorageV2"
+  ip_rules              = var.ip_for_remote_access
+  diag_log_analytics_id = azurerm_log_analytics_workspace.core-la.id
+  #virtual_network_subnet_ids = var.fw_virtual_network_subnet_ids
+  tags = var.tags
+
+  #OPTIONAL
+  public_network_access_enabled = true
+  enable_customer_managed_key   = true
+  cmk_key_vault_id              = module.core_kv.id
+  cmk_key_vault_key_name        = azurerm_key_vault_key.docs-cmk.name
+  storage_containers = [
+    "tfstate"
+  ]
 }

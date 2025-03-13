57c6ef1adb306a27af8b1160efc06440c5dd8e69
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-03-13 14:48:34 -0400

    update image gallery name

--- image_gallery.tf
+++ image_gallery.tf
@@ -1,5 +1,5 @@
 resource "azurerm_shared_image_gallery" "marketplaceimages" {
-  name                = var.compute_gallery_name
+  name                = local.compute_gallery_name
   resource_group_name = azurerm_resource_group.management.name
   location            = var.location
   description         = "Images for FedRAMP Environment"
